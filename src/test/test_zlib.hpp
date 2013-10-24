a774603922c093a6205e6f1130cebd786d4173b1
Marc Gilleron <marc.gilleron@gmail.com>
2013-10-24 22:30:55 +0200

    Included zlib code in project

---
+++ src/test/test_zlib.hpp
@@ -0,0 +1,8 @@
+#ifndef HEADER_TEST_ZLIB_HPP_INCLUDED
+#define HEADER_TEST_ZLIB_HPP_INCLUDED
+
+#include <string>
+
+void test_compress(std::string filePath);
+
+#endif // HEADER_TEST_ZLIB_HPP_INCLUDED
