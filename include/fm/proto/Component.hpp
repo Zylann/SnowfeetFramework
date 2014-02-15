f2aa1933f7e872fabd1da6898d4b8f176684af16
Marc Gilleron <marc.gilleron@gmail.com>
2014-02-15 14:56:08 +0100

    Added formatted documentation to Component.hpp

--- include/fm/proto/Component.hpp
+++ include/fm/proto/Component.hpp
@@ -13,57 +13,73 @@ namespace zn
 
 class Entity;
 
+/// \brief flags that can be set on a component (bitmasks)
 enum ComponentFlags
 {
+	/// \brief the component's update() function hasn't been called yet
 	CF_FIRST_UPDATE = 1
 };
 
-// A piece that composes an entity.
-// Components are a core element of the engine.
-// Some will implement functionnality, others just wrap existing objects
-// to offer a component-friendly interface and engine-specific stuff.
-// Note: components that derive directly from AComponent are often specific to the engine.
-// if you want to implement gameplay, please derive from Behaviour instead.
-// For serialization and execution flow reasons, components must define a default
-// constructor and avoid to rely on a constructor with parameters.
+/// \brief A piece that composes an entity.
+/// Components are a core element of the engine.
+/// Some will implement functionnality, others just wrap existing objects
+/// to offer a component-friendly interface and engine-specific stuff.
+/// Note: components that derive directly from AComponent are often specific to the engine.
+/// if you want to implement gameplay, please derive from Behaviour instead.
+/// For serialization and execution flow reasons, components must define a default
+/// constructor and avoid to rely on a constructor with parameters.
 class ZN_API AComponent
 {
 public:
 
 	virtual ~AComponent();
 
-	// Returns the entity the component belongs to.
+	/// \brief Returns the entity the component belongs to.
+	/// \return the entity
 	Entity & entity() const;
 
-	// Returns the meta-class of the component
+	/// \brief Returns the meta-class of the component
+	/// \return meta-class
 	virtual const ComponentType & componentType() const = 0;
 
-	// Called immediately after the component to be added to an entity.
-	// This method is responsible for referencing the entity.
+	/// \brief Called immediately after the component to be added to an entity.
+	/// This method is responsible for referencing the entity.
 	virtual void onAdd(Entity * e);
 
-	// Called to initialize the component after being added to an entity.
+	/// \brief Called to initialize the component after being added to an entity.
 	virtual void init();
 
-	// Called just before the first update of the component.
+	/// \brief Called just before the first update of the component.
 	virtual void onStart();
 
-	// Called just before the component to be destroyed.
+	/// \brief Called just before the component to be destroyed.
 	virtual void onDestroy();
 
-	// Called when the owning entity receives a generic message
+	/// \brief Called when the owning entity receives a generic message.
+	/// \note this function is very basic, it may change when an event system
+	/// will be implemented.
+	/// \param msg: contents of the message, usually just an identifier
 	virtual void onMessage(const std::string & msg) {}
 
 	//----------
 	// TODO make callbacks below optional by using an event system?
 
-	// Called on each frame to update realtime behaviour.
+	/// \brief Called on each frame to update realtime behaviour.
+	/// this function is not aimed at being override, use onUpdate() instead.
 	void update();
 
-	// Called when the entity starts colliding with something
+	// TODO intergate a physics engine?
+
+	/// \brief Called when the entity starts colliding with something
+	/// \param info: infos about the collision such as the hit point, normal etc.
+	/// \note this function is not currently called by the engine, because
+	/// it has no integrated physics engine yet.
 	virtual void onCollisionEnter(const CollisionInfo & info) {}
 
-	// Called when the entity exits a collision
+	/// \brief Called when the entity exits a collision
+	/// \param info: infos about the collision such as the hit point, normal etc.
+	/// \note this function is not currently called by the engine, because
+	/// it has no integrated physics engine yet.
 	virtual void onCollisionExit(const CollisionInfo & info) {}
 
 	// --------------------
@@ -81,37 +97,49 @@ public:
 	// serialization may be called in a "scene edition" state.
 	// Doing that in "play state" might be useful as a debug dump.
 
-	// Saves the component as JSON data.
-	// Automatically adds type information from componentType().
+	/// \brief Saves the component as JSON data.
+	/// Automatically adds type information from componentType().
+	/// \param component: component to serialize
+	/// \param o: output JSON data tree
 	static void serialize(AComponent * component, JsonBox::Value & o);
 
-	// Creates a component from JSON data
-	// Retrieves type information and use ComponentType factory to instantiate the component.
+	/// \brief Creates a component from its JSON representation
+	/// Retrieves type information and use ComponentType factory to instantiate the component.
+	/// \param o: input JSON data tree
+	/// \return newly created component, or nullptr if something went wrong
 	static AComponent * unserialize(JsonBox::Value & o);
 
-	// Saves component's data to JSON (no need to save the type).
-	// You need to call superclass' version.
+	/// \brief Saves component's properties to JSON (no need to save the class ID).
+	/// When subclassing, you need to call superclass' version.
+	/// \param o: output JSON data tree
 	virtual void serializeData(JsonBox::Value & o);
 
-	// Initializes the component from JSON data
-	// You need to call superclass' version.
+	/// \brief Initializes the component from JSON data
+	/// When subclassing, you need to call superclass' version.
+	/// \param o: input JSON data tree
 	virtual void unserializeData(JsonBox::Value & o);
 
-	// Called after unserialization of all components in the entity.
-	// Use this function to relink references, check integrity etc.
+	/// \brief Called after unserialization of all components in the entity.
+	/// Use this function to relink references, check integrity etc.
+	/// \note this function may change a bit in the future to be more like
+	/// Entity's version.
 	virtual void postUnserialize() {}
 
 protected:
 
-	// Called on each frame to update realtime behaviour.
+	/// \brief Called on each frame to update realtime behaviour.
 	virtual void onUpdate();
 
 	AComponent();
 
 private:
 
-	Entity *  r_owner; // Reference to the entity
-	u8        m_flags;
+	/// \brief reference to the entity that owns the component.
+	Entity * r_owner;
+
+	/// \brief various flags about the state of the component.
+	/// see ComponentFlags.
+	u8 m_flags;
 
 };
 
