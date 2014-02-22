c642d51bd4d4e2ad0ccc4222d6da64480d67e8e4
Marc Gilleron <marc.gilleron@gmail.com>
2014-02-22 17:49:59 +0100

    Improved the Log system a lot

--- include/fm/util/Log.hpp
+++ include/fm/util/Log.hpp
@@ -1,7 +1,7 @@
 /*
 Log.hpp
-Copyright (C) 2010-2013 Marc GILLERON
-This file is part of the zCraftFramework project.
+Copyright (C) 2010-2014 Marc GILLERON
+This file is part of the Plane framework project.
 */
 
 #ifndef HEADER_ZN_LOG_HPP_INCLUDED
@@ -11,55 +11,140 @@ This file is part of the zCraftFramework project.
 #include <string>
 #include <fstream>
 
-#define log_error zn::log << "E: "
-#define log_warning zn::log << "W: "
-#define log_info zn::log << "I: "
-#define log_debug zn::log << "D: "
+#include <fm/types.hpp>
 
 namespace zn
 {
 
-// TODO make it compile properly
-// Simple stream wrapper providing file or console output
+/// \brief Simple stream wrapper providing file and/or console output
 class Log
 {
 public:
 
-	bool openFile(const std::string & fpath)
+	enum MessageTypeMask
 	{
-		m_file.open(fpath.c_str());
-		if(!m_file.good())
-		{
-			std::cout << "E: Log: failed to open file \"" << fpath << '"' << std::endl;
-			return false;
-		}
-		return true;
+		M_DEBUG     = 1,
+		M_INFO      = 1 << 1,
+		M_WARNING   = 1 << 2,
+		M_ERROR     = 1 << 3,
+		M_ALL       = M_DEBUG | M_INFO | M_WARNING | M_ERROR,
+		M_NONE      = 0
+	};
+
+	Log() :
+		m_messageType(M_INFO),
+		m_fileOutputFlags(M_ALL),
+#ifdef ZN_DEBUG
+		m_consoleOutputFlags(M_ALL)
+#else
+		m_consoleOutputFlags(M_WARNING | M_ERROR)
+#endif
+	{}
+
+	/// \brief Sets which types of messages must be written to the file output.
+	/// \param flags: bitmask where set flags activate a type of output.
+	/// \see Log::MessageTypeMask
+	void setFileOutputFlags(u32 flags)
+	{
+		m_fileOutputFlags = flags;
 	}
 
+	/// \brief Sets which types of messages must be written to the console output.
+	/// \param flags: bitmask where set flags activate a type of output.
+	/// \see Log::MessageTypeMask
+	void setConsoleOutputFlags(u32 flags)
+	{
+		m_consoleOutputFlags = flags;
+	}
+
+	/// \brief Sets which file will be used to output log messages.
+	/// \param fpath: path to the file to open
+	/// \return true if the file was successuflly open, false otherwise
+	bool openFile(const std::string & fpath);
+
+	/// \brief Closes the file used to output log messages.
+	/// \note This also disables outputting messages to a file unless openFile() gets called again.
 	void closeFile()
 	{
 		m_file.close();
 	}
 
+	/// \brief Outputs a debug message.
+	/// \return Log itself for chaining on the same line.
+	inline Log & debug()
+	{
+		m_messageType = M_DEBUG;
+		return (*this) << "D: ";
+	}
+
+	/// \brief Outputs an information message.
+	/// \return Log itself for chaining on the same line.
+	inline Log & info()
+	{
+		m_messageType = M_INFO;
+		return (*this) << "I: ";
+	}
+
+	/// \brief Outputs a warning message.
+	/// \return Log itself for chaining on the same line.
+	inline Log & warn()
+	{
+		m_messageType = M_WARNING;
+		return (*this) << "W: ";
+	}
+
+	/// \brief Outputs an error message.
+	/// \return Log itself for chaining on the same line.
+	inline Log & err()
+	{
+		m_messageType = M_DEBUG;
+		return (*this) << "E: ";
+	}
+
+	/// \brief Outputs a message using the current message type.
+	/// \note It is recommended to use specialized output functions instead,
+	/// because each of them end up calling this one.
+	/// \param a: object to print, like with std::cout.
+	/// \return Log itself for chaining on the same line.
 	template <typename T>
 	Log & operator<<(T const & a)
 	{
-		if(m_file)
+		if(m_file && (m_messageType & m_fileOutputFlags))
 		{
 			m_file << a;
 		}
-#ifdef ZN_DEBUG
-		std::cout << a;
-#endif
+
+		if(m_consoleOutputFlags & m_consoleOutputFlags)
+		{
+			std::cout << a;
+		}
+
 		return *this;
 	}
 
 private:
 
+	/// \brief File where messages can be output.
 	std::ofstream m_file;
+
+	/// \brief Type of the last message.
+	MessageTypeMask m_messageType;
+
+	/// \brief Bitmask storing which messages to output to the file.
+	u8 m_fileOutputFlags;
+
+	/// \brief Bitmask storing which messages to output to the console.
+	u8 m_consoleOutputFlags;
 
 };
 
+/// \brief Appends an end-of-line character to a log message, also indicating the end of your message.
+/// Use this flux manipulator at the end of your log.* message calls.
+/// \note You might often specify the full namespace to use it (zn::endl).
+Log & endl(Log & l);
+
+/// \brief Global log variable, for convenience. Behaves like std::cout.
+/// \note It is not assumed to exist in Log objects, so you can create other instances without problems.
 extern Log log;
 
 } // namespace zn
--- src/fm/util/Log.cpp
+++ src/fm/util/Log.cpp
@@ -5,5 +5,24 @@ namespace zn
 
 Log log;
 
+//------------------------------------------------------------------------------
+bool Log::openFile(const std::string & fpath)
+{
+	m_file.open(fpath.c_str());
+	if(!m_file.good())
+	{
+		std::cout << "E: Log: failed to open file \"" << fpath << '"' << std::endl;
+		return false;
+	}
+	return true;
+}
+
+//------------------------------------------------------------------------------
+Log & endl(Log & l)
+{
+	l << '\n';
+	return l;
+}
+
 } // namespace zn
 
