ae3bf3bd6838ad87138c57b631b8846b718c25bc
Marc Gilleron <marc.gilleron@gmail.com>
2014-02-23 18:16:22 +0100

    AssetBank documentation

--- include/fm/asset/AssetBank.hpp
+++ include/fm/asset/AssetBank.hpp
@@ -28,34 +28,65 @@ class ZN_API AssetBank : public NonCopyable
 {
 public:
 
-	// TODO complete doc
+	/// \brief Contains all loaded textures.
+	AssetMap<sf::Texture> textures;
 
-	AssetMap<sf::Texture>      textures;
-	AssetMap<sf::Shader>       shaders;
-	AssetMap<sf::Font>         fonts;
-	AssetMap<TextureAtlas>     atlases;
-	AssetMap<TiledMap>         maps;
-	AssetMap<sf::SoundBuffer>  soundBuffers; // TODO AssetBank: merge sound types into one
-	AssetMap<FileRef>          soundStreams;
-	AssetMap<Material>         materials;
+	/// \brief Contains all loaded shaders.
+	AssetMap<sf::Shader> shaders;
 
+	/// \brief Contains all loaded fonts.
+	AssetMap<sf::Font> fonts;
+
+	/// \brief Contains all loaded atlases.
+	AssetMap<TextureAtlas> atlases;
+
+	/// \brief Contains all loaded maps.
+	AssetMap<TiledMap> maps;
+
+	/// \brief Contains all loaded sounds.
+	AssetMap<sf::SoundBuffer> soundBuffers; // TODO AssetBank: merge sound types into one
+
+	/// \brief Contains all streamed sounds as file references.
+	AssetMap<FileRef> soundStreams;
+
+	/// \brief Contains all loaded materials.
+	AssetMap<Material> materials;
+
+	/// \brief Constructs an empty AssetBank.
 	AssetBank();
+
+	/// \brief Destroys the AssetBank and frees all the assets it contains.
 	~AssetBank();
 
-	/// \brief Open or creates and indexes asset manifests, then loads assets contained in a folder.
+	/// \brief Given a root folder, loads every asset contained in it by reading a special manifest file.
+	/// if the manifest is not present, the folder can be scanned for asset files and the manifest will be generated.
+	/// Asset file recognition works with pattern matching applided on file paths.
+	/// Patterns are loaded from a second special file. If it is not found, a default one will be created.
 	/// \param rootFolder: root folder of the assets
-	/// \param performAutoIndex: recursive search for files to add to the manifest, based on file associations
-	/// \return true if success, false otherwise
+	/// \param performAutoIndex: enables folder scanning if no manifest is found, or if you want to update the existing one.
+	/// \return true if success, false if an error occurred.
 	bool load(const std::string & rootFolder, bool performAutoIndex=false);
 
-	// Sets the root folder containing all the assets.
-	// This folder will be prepended to every assets to be loaded.
+	/// \brief Sets the root folder containing all the assets.
+	/// This folder will be prepended to every assets to be loaded.
+	/// \note this function is called by load().
 	void setRootFolder(const std::string & rootFolder);
 
+	/// \brief Scans the root folder and its subdirectories for asset files,
+	/// and puts them in a manifest.
+	/// \param folderPath: the root folder to scan, usually the one from this AssetBank.
+	/// \param manifest: manifest file to fill
+	/// \return true if succeeded, false if an error occurred
 	bool autoIndex(const std::string & folderPath, AssetBankManifest & manifest);
 
+	/// \brief Effectively loads all assets described in the given manifest.
+	/// \param manifest: list of files to load
+	/// \return true if every asset has been loaded, false if one failed.
 	bool loadAssets(AssetBankManifest & manifest);
 
+	/// \brief Finds the asset type associated to the given file.
+	/// \param filePath: path to the file
+	/// \return AssetMap associated with the file
 	const AssetMapBase * findAssetType(const std::string & filePath);
 
 	/// \brief Loads file-extension <--> asset-type associations from a JSON file.
@@ -66,10 +97,10 @@ public:
 	/// \return true if success, false if error.
 	bool loadFileAssociations(const std::string & assocFile, bool create);
 
-	// Sets this AssetBank as the current one for global access
+	/// \brief Sets this AssetBank as the current one for global access
 	void makeCurrent();
 
-	// Gets the active AssetBank
+	/// \brief Gets the active AssetBank
 	static AssetBank * current();
 
 private:
@@ -81,6 +112,7 @@ private:
 	/// \brief Files matching this will be ignored first.
 	AssetFileMatcher m_ignoredFilesMatcher;
 
+	/// \brief root folder of the AssetBank, relative to the execution's folder.
 	std::string m_root;
 
 	/// \brief References all asset containers under the same map type for bulk operations
